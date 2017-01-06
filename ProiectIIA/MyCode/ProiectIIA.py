c8c067ea76b53c2b922e91ebfbaaa443e06fb700
tandura <phi_brain1000@yahoo.com>
2017-01-06 19:06:45 +0200

    Forgot to commit on progress. Done. :)

--- ProiectIIA/MyCode/ProiectIIA.py
+++ ProiectIIA/MyCode/ProiectIIA.py
@@ -4,7 +4,6 @@ import inspect
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-print(parentdir)
 sys.path.insert(0,parentdir) 
 
 if sys.version_info < (3, 0):
@@ -13,73 +12,241 @@ if sys.version_info < (3, 0):
 from utils import *
 from logic import *
 
-Sex = {}
-Sex['M'] = Symbol("Sex[M]")
-Sex['F'] = Symbol("Sex[F]")
+simptomsTag = "Pacientul prezinta simptomele:"
+riscTags = "Pacientul prezinta urmatorii factori de risc:"
+testTag = "Teste:"
+diagnosticTag = "Diagnostic:"
 
-Age = {}
-Age['49_L'] = Symbol("Age[49_L]")
-Age['50_59'] = Symbol("Age[50_59]")
-Age['60_69'] = Symbol("Age[60_69]")
-Age['70_O'] = Symbol("Age[70_O]")
+SimptomsValues = {
+                    "poliuria": 15, # cele mai comune
+                    "polidipsia": 15,
+                    "polifagia": 15,
+                    "scadere in greutate": 15,
+                    "situatii repetate de vedere incetosata":  8, # mai putin comune
+                    "mancarime":  8,
+                    "deteriorarea vaselor sangvine":  8,
+                    "infectii vaginale recurente":  8,
+                    "stare accentuata de oboseala":  8
+                 }
 
-EthincBack = {}
-EthincBack['White_Europ'] = Symbol("EthincBack[White_Europ]")
-EthincBack['Other'] = Symbol("EthincBack[Other]")
+RiscValues = {
+                "Dieta bogata in zahar": 20,
+                "Stilul de viata sedentar": 20,
+                "Greutate mica la nastere": 20,
+                "Varsta": 20,
+                "Antecedentele din familie": 20
+             }
 
-History = {}
-History['yes'] = Symbol("History[yes]")
-History['no'] = Symbol("History[no]")
+AcceptedTests = ["FPG", "A1C", "OGTT", "RPG"]
+AcceptedDiagnostics = ["Prediabetes", "Diabet", "Nu are diabet"]
+IrelevantTests = []
+IrelevantSimptoms = []
+IrelevantRiscs = []
 
-Waist = {}
-Waist['90_L'] = Symbol("Waist[90_L]")
-Waist['90_99'] = Symbol("Waist[90_99]")
-Waist['100_109'] = Symbol("Waist[100_109]")
-Waist['110_G'] = Symbol("Waist[110_G]")
+IrelevantDiagnostic = False
+noTestsDone = True
 
-BMI = {}
-BMI['25_L'] = Symbol("BMI[25_L]")
-BMI['25_29'] = Symbol("BMI[25_29]")
-BMI['30_35'] = Symbol("BMI[30_35]")
-BMI['35_O'] = Symbol("BMI[35_O]")
+A1C = {}
+A1C["Normal"] = Symbol("A1C_Normal")
+A1C["Prediabetes"] = Symbol("A1C_Prediabetes")
+A1C["Diabetes"] = Symbol("A1C_Diabetes")
 
-Medication = {}
-Medication['yes'] = Symbol("Medication[yes]")
-Medication['no'] = Symbol("Medication[no]")
+FPG = {}
+FPG["Normal"] = Symbol("FPG_Normal")
+FPG["Prediabetes"] = Symbol("FPG_Prediabetes")
+FPG["Diabetes"] = Symbol("FPG_Diabetes")
 
-diabetes = {}
-diabetes['yes'] = Symbol("Diabetes[yes]")
-diabetes['no'] = Symbol("Diabetes[no]")
+OGTT = {}
+OGTT["Normal"] = Symbol("OGTT_Normal")
+OGTT["Prediabetes"] = Symbol("OGTT_Prediabetes")
+OGTT["Diabetes"] = Symbol("OGTT_Diabetes")
+
+RPG = {}
+RPG["Diabetes"] = Symbol("RPG_Diabetes")
+
+Diabetes = Symbol("Diabetes")
+Prediabetes = Symbol("Prediabetes")
+
+def readDefaultInputFile():
+    with open(r".\MyCode\DefaultInput.txt") as f:
+        #print("File content:")
+        #content = ""
+        content = f.read()
+        
+        #check for "IGNORE" tag
+        position = content.find("#IGNORE#", 0)
+        if position != -1:
+            content = content[0:position]
+        #print(content + "\n\n")
+        
+        return content
+
+def splitList(List):
+    List = List.replace(".", ",")
+    List = List.replace(";", ",")
+    List = List.split(",")
+    Results = []
+    for elem in List:
+        if elem.strip() != "":        
+            Results.append(elem.strip())
+
+    return Results
+    
+
+def parseContent(content):
+    global noTestsDone, IrelevantTests
+
+    positionSimptom = content.find(simptomsTag, 0) #get tag positions
+    positionRisc = content.find(riscTags, positionSimptom)
+    positionTests = content.find(testTag, positionRisc)
+    positionDiagnostic = content.find(diagnosticTag, positionTests)
+
+    simptomsList = content[positionSimptom + len(simptomsTag):positionRisc]
+    simptomsList = splitList(simptomsList)
+    #print("Simptoms:")
+    #for simptom in simptomsList:        
+    #    print("\t" + simptom)
+
+    riscsList = content[positionRisc + len(riscTags) : positionTests]
+    riscsList = splitList(riscsList)
+    #print("Riscs:")
+    #for risc in riscsList:        
+    #    print("\t" + risc)
+
+    testList = content[positionTests + len(testTag) : positionDiagnostic]
+    testList = testList.replace(";", ",")
+    testList = testList.split(",")
+    Results = []
+    for elem in testList:
+        if elem.strip() != "":        
+            Results.append(elem.strip())
+    testList = Results
+    #print("Tests:")
+    #for test in testList:        
+    #    print("\t" + test)
+
+    testDict = {}
+    if testList[0].strip('.') != "NONE":
+        for test in testList:
+            test = test.split(" ")
+            if test[0] in AcceptedTests:
+                testDict[test[0]] = float(test[1])
+            else:
+                IrelevantTests.append(test[0])
+        
+        if len(testDict.keys()) > 0:
+            noTestsDone = False
+    
+    diagnostic = content[positionDiagnostic + len(diagnosticTag) : ].strip()
+    diagnostic = diagnostic.replace(".", "")
+    #print("Diagnostic:\n\t" + diagnostic)
+
+    return (simptomsList, riscsList, testDict, diagnostic)
 
 def InitKB():
     kb = PropKB()
 
-    kb.tell(Sex['M'])
-    kb.tell(EthincBack['White_Europ'])
-    kb.tell(Sex['M'] | '==>' | ~Sex['F'])
-    kb.tell(Sex['F'] | '==>' | ~Sex['M'])
-    kb.tell(Sex['M'] & EthincBack['White_Europ'] | '==>' | diabetes['yes'])
+    kb.tell(A1C["Diabetes"] | FPG["Diabetes"] | OGTT["Diabetes"] | RPG["Diabetes"] | '==>' | Diabetes)
+    kb.tell(A1C["Prediabetes"] | FPG["Prediabetes"] | OGTT["Prediabetes"] | '==>' | Prediabetes)
 
     return kb
 
+def processData(simptomsList, riscsList, testDict, diagnostic, kb):
+    global IrelevantSimptoms, IrelevantRiscs
+    
+    Score = 0
+    
+    for simptom in simptomsList:
+        if simptom not in SimptomsValues.keys():
+            IrelevantSimptoms.append(simptom)
+        else:
+            Score += SimptomsValues[simptom]
 
-def askKB(kb):
-    rez = kb.ask(Sex['M'])
-    if rez == {}:
-        print("The pacient is male!")
+    for risc in riscsList:
+        if risc not in RiscValues.keys():
+            IrelevantRiscs.append(risc)
+        else:
+            Score += RiscValues[risc]
 
-    rez = kb.ask(~Sex['F'])
-    if rez == {}:
-        print("The pacient is not female!")
+    if "FPG" in testDict.keys():
+        if testDict["FPG"] < 5.6: 
+            kb.tell(FPG["Normal"])
+        else:
+            if testDict["FPG"] >= 5.6 and  testDict["FPG"] <= 6.4:
+                kb.tell(FPG["Prediabetes"])
+            else:
+                kb.tell(FPG["Diabetes"])
 
-    rez = kb.ask(diabetes['yes'])
-    if rez == {}:
-        print("The pacient has diabetes!")
+    if "A1C" in testDict.keys():
+        if testDict["A1C"] < 99: 
+            kb.tell(A1C["Normal"])
+        else:
+            if testDict["A1C"] >= 100 and  testDict["A1C"] <= 125:
+                kb.tell(A1C["Prediabetes"])
+            else:
+                kb.tell(A1C["Diabetes"])
+
+    if "OGTT" in testDict.keys():
+        if testDict["OGTT"] < 139: 
+            kb.tell(OGTT["Normal"])
+        else:
+            if testDict["OGTT"] >= 140 and  testDict["OGTT"] <= 199:
+                kb.tell(OGTT["Prediabetes"])
+            else:
+                kb.tell(OGTT["Diabetes"])
+
+    if "RPG" in testDict.keys():
+        if testDict["RPG"] >= 200: 
+            kb.tell(RPG["Diabetes"])
+
+    return (Score, kb)
+
+def interpretResults(kb, diagnostic, Score):
+    flag = False;
+
+    if Score <= 40 and noTestsDone == False:
+        print("The simptoms and riscs does not suport the keed for tests!") 
+
+    if noTestsDone and diagnostic != "None":
+        print("No definitive diagnostic can be given! Recomandation: do some tests.")       
     else:
-        print("The pacient does not have diabetes!")
+        rez = kb.ask(Diabetes)
+        if rez == {}:
+            print("The correct diagnostic is: Diabetes.")
+            flag = True
+
+        rez = kb.ask(Prediabetes)
+        if rez == {}:
+            print("The correct diagnostic is: Prediabetes.")
+            flag = True
+
+        if flag == False:
+            print("The correct diagnostic is: No diabetes.")
+
+        print("Your diacnostic is: " + diagnostic)
+
+    if len(IrelevantSimptoms) > 0:
+        print("The following symptom(s) did not influence the decision:")
+        for symptom in IrelevantSimptoms:
+            print(symptom)
+
+    if len(IrelevantRiscs) > 0:
+        print("The following risk(s) did not influence the decision:")
+        for risk in IrelevantRiscs:
+            print(risk)
+
+    if len(IrelevantTests) > 0:
+        print("The following test(s) did not influence the decision:")
+        for test in IrelevantTests:
+            print(test)
+    
 
 def main():
-    askKB(InitKB())
+    content = readDefaultInputFile()
+    kb = InitKB()
+    (simptomsList, riscsList, testDict, diagnostic) = parseContent(content)
+    Score, kb = processData(simptomsList, riscsList, testDict, diagnostic, kb)
+    interpretResults(kb, diagnostic, Score)
 
 if __name__ == '__main__':
     main()
